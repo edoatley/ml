70787c848878d986ae2c398a231eb084dd046e93
edoatleynewday <ed.oatley@newday.co.uk>
2024-07-02 17:06:34 +0100

    seems to work but image pull issue

---
+++ kubeflow/resources/minimal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo snap refresh juju --channel=stableapt update -y && apt upgrade -y
--- kubeflow/resources/userdata.sh
+++ kubeflow/resources/userdata.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-UserRunAs='azureuser'
-KubernetesVersion='1.30'
-KubeflowVersion='1.8'
-KubeflowDashboardUsername='azureuser@example.com'
-KubeflowDashboardPassword='AzureUser123!'
+# This is a basic installation script that will install minikube and kubeflow on a VM
 
-# Package management
-apt update
-apt -y upgrade
+# Install docker
+apt update -y && apt upgrade -y
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo apt install -y apt-transport-https ca-certificates curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# Install Minikube
+# Install CLI tools kubectl and kustomize
+sudo snap install kubectl --classic
+sudo snap install kustomize
+
+# Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-alias kubectl="minikube kubectl --"
+minikube start --driver=docker
+minikube status
 
-curl -s https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh  | bash
-sudo install kustomize /usr/local/bin/kustomize
-
-
-git clone https://github.com/kubeflow/manifests.git
-cd manifests
+git clone https://github.com/kubeflow/manifests.git ~/tmp/kubeflow
+cd ~/tmp/kubeflow
 while ! kustomize build example | awk '!/well-defined/' | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
-
+# rm -rf ~/tmp/kubeflow # could clean up with this
